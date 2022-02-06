d0b549000764de149f4c36779b6b2cdd83c4a6e3
levansen <senevirl@sheridancollege.ca>
2022-02-05 21:30:47 -0500

    Sound should be enabled when pressed on the piano tile.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(rainbowPianoApp());
 
-class XylophoneApp extends StatelessWidget {
+class rainbowPianoApp extends StatelessWidget {
 
   void numPlay(int num){
     final player = AudioCache();
@@ -13,4 +12,43 @@ class XylophoneApp extends StatelessWidget {
 
 
   }
+
+  Expanded playSound({ required Color color, required int soundNum}){
+
+    return Expanded(
+      child: FlatButton(
+        color: color,
+
+        onPressed: (){
+
+          numPlay(soundNum);
+        }, child: Text('.'),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              playSound(color: Colors.red, soundNum: 1),
+              playSound(color: Colors.blue,soundNum: 2),
+              playSound(color: Colors.pink,soundNum: 3),
+              playSound(color: Colors.purple,soundNum: 4),
+              playSound(color: Colors.lightGreen, soundNum:5),
+              playSound(color: Colors.yellow,soundNum: 6),
+              playSound(color: Colors.brown, soundNum:7),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
